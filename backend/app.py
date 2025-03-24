b633d5437c404611e306cb0abad9d065959b2520
Rosie Babbra <rosiebabbra@gmail.com>
2025-03-24 09:42:53 -0500

    Update reference

--- backend/app.py
+++ backend/app.py
@@ -13,9 +13,7 @@ CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    f"sqlite:///{os.path.join(basedir, 'var/app-instance/accounting.db')}"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounting.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
